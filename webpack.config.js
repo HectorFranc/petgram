b4acf05acc84c8a917a19b37caf62f7aa2cced22
Hector Franco T <tor6.franco6@hotmail.com>
2020-07-04 16:18:51 -0500

    React env

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,30 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+    entry: './src/index.jsx',
     output: {
         filename: 'app.bundle.js',
     },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-react',
+                        ]
+                    }
+                }
+            }
+        ]
+    },
     plugins: [
-        new HtmlWebpackPlugin()
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        })
     ]
 }
