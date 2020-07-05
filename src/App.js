198e9fe666abfb5b5227bd6b8f7c4305fd497144
Hector Franco T <tor6.franco6@hotmail.com>
2020-07-05 13:40:58 -0500

    Svg Logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import { Logo } from './components/Logo'
 import { ListOfCategories } from './components/ListOfCategories'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
 
@@ -8,6 +9,7 @@ import { GlobalStyles } from './GlobalStyles'
 export const App = () => (
   <>
     <GlobalStyles />
+    <Logo />
     <ListOfCategories path='/' />
     <ListOfPhotoCards />
   </>
