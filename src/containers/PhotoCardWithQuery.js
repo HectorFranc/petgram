2794ed9c29d33cbe9bea87ac9a9cb8eb4a89ce96
Hector Franco T <tor6.franco6@hotmail.com>
2020-07-14 14:23:02 -0500

    Query refactoring

--- src/containers/PhotoCardWithQuery.js
+++ src/containers/PhotoCardWithQuery.js
@@ -4,7 +4,7 @@ import { PhotoCard } from '../components/PhotoCard'
 import { gql } from 'apollo-boost'
 import { Query } from 'react-apollo'
 
-const query = gql`
+const GET_SINGLE_PHOTO = gql`
   query getSinglePhoto($id: ID!) {
     photo(id: $id) {
       id
@@ -17,16 +17,20 @@ const query = gql`
   }
 `
 
+const renderProps = ({ data, error, loading }) => {
+  if (error) return <p>Error!</p>
+
+  if (loading) return <p>Loading...</p>
+
+  const { photo = {} } = data
+
+  return <PhotoCard {...photo} />
+}
+
 export const PhotoCardWithQuery = ({ id }) => {
   return (
-    <Query query={query} variables={{ id }}>
-      {
-        ({ data }) => {
-          const { photo = {} } = data || {}
-
-          return <PhotoCard {...photo} />
-        }
-      }
+    <Query query={GET_SINGLE_PHOTO} variables={{ id }}>
+      {renderProps}
     </Query>
   )
 }
--- src/hoc/withPhotos.js
+++ src/hoc/withPhotos.js
@@ -1,7 +1,7 @@
 import { graphql } from 'react-apollo'
 import { gql } from 'apollo-boost'
 
-export const withPhotos = graphql(gql`
+const GET_PHOTOS = gql`
   query getPhotos($categoryId: ID) {
     photos(categoryId: $categoryId) {
       id
@@ -10,6 +10,8 @@ export const withPhotos = graphql(gql`
       likes
       userId
       liked
-      }
+    }
   }
-`)
+`
+
+export const withPhotos = graphql(GET_PHOTOS)
