7709cd4958643afda873e4e189faba92d59915f8
Hector Franco T <tor6.franco6@hotmail.com>
2020-07-13 22:07:59 -0500

    Graphql apollo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
 import ReactDOM from 'react-dom'
+
+const client = new ApolloClient({
+  uri: 'https://petgram-server.hectorf.vercel.app/graphql'
+})
 
 import { App } from './App'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>, document.getElementById('app'))
