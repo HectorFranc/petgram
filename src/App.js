33b960a766e4bf44f3f5e1e0b8aea6254f7332ba
Hector Franco T <tor6.franco6@hotmail.com>
2020-07-14 14:14:19 -0500

    Render props with Query element

--- src/App.js
+++ src/App.js
@@ -3,14 +3,26 @@ import React from 'react'
 import { Logo } from './components/Logo'
 import { ListOfCategories } from './components/ListOfCategories'
 import { ListOfPhotoCards } from './containers/ListOfPhotoCards'
+import { PhotoCardWithQuery } from './containers/PhotoCardWithQuery'
 
 import { GlobalStyles } from './styles/GlobalStyles'
 
-export const App = () => (
-  <>
-    <GlobalStyles />
-    <Logo />
-    <ListOfCategories path='/' />
-    <ListOfPhotoCards />
-  </>
-)
+export const App = () => {
+  const urlParams = new window.URLSearchParams(window.location.search)
+  const detailId = urlParams.get('detail')
+
+  return (
+    <>
+      <GlobalStyles />
+      <Logo />
+      {
+        detailId
+          ? <PhotoCardWithQuery id={detailId}/>
+          : <>
+            <ListOfCategories path='/' />
+            <ListOfPhotoCards />
+          </>
+      }
+    </>
+  )
+}
---
+++ src/containers/PhotoCardWithQuery.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { PhotoCard } from '../components/PhotoCard'
+
+import { gql } from 'apollo-boost'
+import { Query } from 'react-apollo'
+
+const query = gql`
+  query getSinglePhoto($id: ID!) {
+    photo(id: $id) {
+      id
+      categoryId
+      src
+      likes
+      userId
+      liked
+    }
+  }
+`
+
+export const PhotoCardWithQuery = ({ id }) => {
+  return (
+    <Query query={query} variables={{ id }}>
+      {
+        ({ data }) => {
+          const { photo = {} } = data || {}
+
+          return <PhotoCard {...photo} />
+        }
+      }
+    </Query>
+  )
+}
