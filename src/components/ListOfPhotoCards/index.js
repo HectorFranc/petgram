f5115b04a3339b21fc09eb4c3d1ab7980df00a2a
Hector Franco T <tor6.franco6@hotmail.com>
2020-07-13 22:27:24 -0500

    Query parameters

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,29 +1,11 @@
 import React from 'react'
 
-import { graphql } from 'react-apollo'
-import { gql } from 'apollo-boost'
-
 import { PhotoCard } from '../PhotoCard'
 
-const withPhotos = graphql(gql `
-  query getPhotos {
-    photos {
-      id
-      categoryId
-      src
-      likes
-      userId
-      liked
-      }
-  }
-`)
-
-const ListOfPhotoCardsComponent = ({ data: { photos = [] } } = {}) => {
+export const ListOfPhotoCardsComponent = ({ data: { photos = [] } } = {}) => {
   return (
     <ul>
       {photos.map(photo => <PhotoCard key={photo.id} {...photo} />)}
     </ul>
   )
 }
-
-export const ListOfPhotoCards = withPhotos(ListOfPhotoCardsComponent)
