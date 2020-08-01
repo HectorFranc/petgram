8ce1c86006f7396d35988a86e698a639d0b16922
Hector Franco T <tor6.franco6@hotmail.com>
2020-08-01 14:13:23 -0500

    Get Favs. Fetch policy.

--- src/pages/Favs.js
+++ src/pages/Favs.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
+import { FavsWithQuery } from '../containers/GetFavorites'
+
 export const Favs = () => {
   return (
-    <h1>Favs</h1>
+    <>
+      <h1>Favs</h1>
+      <FavsWithQuery />
+    </>
   )
 }
