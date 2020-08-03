3a005ee13e128b720f310e999e8c42c8bc999918
Hector Franco T <tor6.franco6@hotmail.com>
2020-08-03 14:30:12 -0500

    PropTypes

--- src/components/FavButton/index.js
+++ src/components/FavButton/index.js
@@ -3,6 +3,8 @@ import React from 'react'
 import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
 import { Button } from './styles'
 
+import PropTypes from 'prop-types'
+
 export const FavButton = ({ likes, liked, onClick }) => {
   const Icon = liked ? MdFavorite : MdFavoriteBorder
   return (
@@ -10,4 +12,10 @@ export const FavButton = ({ likes, liked, onClick }) => {
       <Icon size='32px' /> {likes} likes!
     </Button>
   )
+}
+
+FavButton.proptypes = {
+  likes: PropTypes.number.isRequired,
+  liked: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired
 }
