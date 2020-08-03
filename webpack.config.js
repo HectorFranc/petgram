533f3b3093d6e6ced72b58162c1ed313f15b36db
Hector Franco T <tor6.franco6@hotmail.com>
2020-08-03 14:43:52 -0500

    Manifest generation

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackPwaManifestPlugin = require('webpack-pwa-manifest')
+const path = require('path')
 
 module.exports = {
   output: {
@@ -25,6 +27,19 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html'
+    }),
+    new WebpackPwaManifestPlugin({
+      name: 'Petgram - Tu app de mascotas',
+      short_name: 'Petgram',
+      description: 'Encuentra fotos de animales',
+      background_color: '#fff',
+      theme_color: '#b1a',
+      icons: [
+        {
+          src: path.resolve('src/assets/icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512]
+        }
+      ]
     })
   ]
 }
