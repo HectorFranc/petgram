d2e52e2936be9375e01c9f73adcc781c14122039
Hector Franco T <tor6.franco6@hotmail.com>
2020-08-03 14:13:06 -0500

    React.memo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import { ListOfCategories } from '../components/ListOfCategories'
 import { ListOfPhotoCards } from '../containers/ListOfPhotoCards'
 import { Layout } from '../components/Layout'
 
-export const Home = ({ id }) => {
+const HomePage = ({ id }) => {
   return (
     <Layout title='Tu app de fotos de animales' subtitle='Encuentra fotos de animales'>
       <ListOfCategories path='/' />
@@ -12,3 +12,7 @@ export const Home = ({ id }) => {
     </Layout>
   )
 }
+
+export const Home = React.memo(HomePage, (prev, next) => {
+  return prev.id === next.id
+})
